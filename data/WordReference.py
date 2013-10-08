276d97184cb32344219b6c706ae16f348d357e8c
Peter Meckiffe <peterf.meckiffe@gmail.com>
2013-10-08 21:37:41 +0100

    UTF issues resolved, and repetitions removed from output

--- data/WordReference.py
+++ data/WordReference.py
@@ -1,28 +1,27 @@
 # coding=UTF-8
 import json
 from urllib2 import urlopen
-import urllib2
-import re
+import urllib2, re
+
+from encodingtools import *
 
 class WordReferenceDefinition(object):
 	# Readable flag, set if you want to be able to understand ANYTHING
 	READABLE = False
 	def getWordDefinitionAsDict(self, word, dictionary_code):
-		print word
-		url_word = word# urllib2.quote(word)
-		if isinstance(dictionary_code, unicode):
-			print "woohoo"
-		url = u"http://api.wordreference.com/0.8/8a8bc/json/"+dictionary_code+u'/'+url_word
-		print url
+		# Use quote method to percent encode, after first encoding utf with hex
+		url_word = urllib2.quote(word.encode('utf-8'))
+		url = "http://api.wordreference.com/0.8/8a8bc/json/%s/%s" %(dictionary_code, url_word)
 		try:
-			result = urlopen(url).read().encode('utf-8').rstrip()
-			print result
+			# Get the JSON from the web server
+			result = urlopen(url).read().rstrip()
 			return json.loads(result)
 		except urllib2.URLError, e:
 			print url
 			return None
 	def getWordDefinitionWithCheck(self, word, dictionary_code):
 		definition = self.getWordDefinitionAsDict(word, dictionary_code)
+		# The first 3 here all me the word isn't valid
 		if definition is None:
 			return None
 		elif len(definition) <= 2:
@@ -35,9 +34,12 @@ class WordReferenceDefinition(object):
 			else:
 				return definition
 	def _makeReadable(self,word, definition):
-		print word
+		# The structure of the dictionary
 		structure = {"word":word,"translations":list(),"compound":list()}
-		# Compute translations
+		# These variables act as a mechanism to check that a translation is not repeated
+		translation_set = set()
+		compound_set = set()
+		# This is just taking the convoluted word reference JSON and making it simpler
 		for category in definition:
 			if re.match("term\d",category):
 				for name in definition[category]:
@@ -47,11 +49,24 @@ class WordReferenceDefinition(object):
 							trans["translation"]=definition[category][name][tr]["FirstTranslation"]["term"]
 						else:
 							trans["translation"]="None available"
-						structure["translations"].append(trans)
+						# These few lines prevent repetitions by not allowing any that don't expand a set
+						# (And are therefore already existent in the set)
+						before = len(translation_set)
+						translation_set.add(trans["original"]+trans["translation"])
+						after = len(translation_set)
+						if before < after:
+							structure["translations"].append(trans)
 			elif re.match("original", category):
 				for number in definition[category]["Compounds"]:
 					compound = {"original":definition[category]["Compounds"][number]["OriginalTerm"]["term"],"translation":definition[category]["Compounds"][number]["FirstTranslation"]["term"]}
-					structure["compound"].append(compound)
+					
+					# These few lines prevent repetitions by not allowing any that don't expand a set
+					# (And are therefore already existent in the set)
+					before = len(compound_set)
+					compound_set.add(compound["original"]+compound["translation"])
+					after = len(compound_set)
+					if before < after:
+						structure["compound"].append(compound)
 		return structure
 
 
@@ -59,4 +74,4 @@ if __name__ == '__main__':
 	wr = WordReferenceDefinition()
 	wr.READABLE=True
 
-	print wr.getWordDefinitionWithCheck("apolíneo", "esen")['translations'][0]['original']
+	print wr.getWordDefinitionWithCheck("hijito", "esen")
